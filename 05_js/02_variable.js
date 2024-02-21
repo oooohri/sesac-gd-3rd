c6061f491c1da72f22a7a5d273e884be9200374d
oooohri <oooorrrr0117@gmail.com>
2024-02-21 16:59:35 +0900

    study 240221 - bootstrap,js

---
+++ 05_js/02_variable.js
@@ -0,0 +1,55 @@
+console.log("connected!");
+
+// * 변수 선언하고 사용하기
+
+/* 
+    var 키워드 
+    1. 변수 선언 -> 값 할당
+    2. 변수 선언과 값 할당을 동시에
+ */
+
+// ! 변수 선언
+var varName;
+
+// ! 변수 할당
+varName = "layla";
+console.log(varName); // layla 출력
+
+// ! 변수 재할당
+varName = "Yuki"; // varName 변수의 값이 "layla" -> "Yuki" 변경
+console.log(varName); // Yuki 출력
+
+// ! 변수 선언과 값 할당을 동시에
+var varName2 = "layla2";
+
+// ! var의 이상한 점
+// 1. 변수를 동일한 이름으로 재선언할 수 있음
+var varName = "lily";
+console.log(varName);
+
+// 2. 변수를 선언하기 전 값을 할당할 수 있음
+// 장점같아 보일 수 있으나, 유지보수 할 때 최악
+aa = 123;
+console.log(aa);
+
+/*
+    let 키워드
+    1. 변수 선언 -> 값 할당
+    2. 변수 선언과 값 할당을 동시에
+ */
+
+// ! 변수 선언
+let letName;
+
+// ! 값 할당
+letName = "홍길동";
+console.log(letName);
+
+// ! 변수 선언과 값 할당 동시에
+let letName2 = "심청이";
+console.log(letName2);
+
+// let letName2 = "심봉사"; // error : 중복 선언 불가
+
+letName2 = "심봉사"; // 값 재할당은 가능
+console.log(letName2);
